ac0f6ee8733acb92224337dc3ede44a3653309ce
bouhenni1982 <bouhenni1982@users.noreply.github.com>
2026-03-19 22:57:40 +0100

    Fix errors and setup Windows CI Workflow

---
+++ push.bat
@@ -0,0 +1,6 @@
+git status
+git add .
+git commit -m "Fix errors and setup Windows CI Workflow"
+git branch -M main
+git remote add origin https://github.com/bouhenni1982/Accessible-Office-Installer.git
+git push -u origin main
